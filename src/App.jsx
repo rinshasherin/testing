99b6d48038f8d2ed5b060a53ed91ca9c90c7da02
rinshasherin <rinshasherinck2002@gmail.com>
2025-02-19 11:41:20 +0530

    Added new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Welcome from './Welcome'
 
 function App() {
 
   return (
     <>
       <h2>Testing</h2>
+      <Welcome />
     </>
   )
 }
